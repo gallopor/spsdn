0965f1fb23352cfe33ba8ce3dc167c59a1a2f1c6
jxie <gallopor@qq.com>
2021-03-25 21:42:40 +0800

    0325

--- GraphAL.py
+++ GraphAL.py
@@ -1,12 +1,26 @@
 class Vertex:
-    def __init__(self, id, **kwargs):
-        self._id = id
-        self.properties = kwargs       # 节点属性
-        self.neighbors = dict()        # 相邻节点
+    def __init__(self, name, properties=None):
+        self._name = name
+        self._properties = properties  # 节点属性(dict)
+        self._neighbors = dict()       # 相邻节点
 
     @property
-    def id(self):
-        return self._id
+    def name(self):
+        return self._name
+
+    @property
+    def properties(self):
+        return self._properties
+
+    @property
+    def neighbors(self):
+        '''
+        返回该节点所有邻居节点排序后的名称列表
+        '''
+        return sorted(list(self._neighbors.keys()))
+
+    def __repr__(self):
+        return str(self.name)
 
     def add_neighbor(self, vertex, weight=0):
         '''
@@ -15,77 +29,62 @@ class Vertex:
         :param weight: 权重，默认值为0
         '''
         if isinstance(vertex, Vertex):
-            if vertex.id not in self.neighbors:
-                self.neighbors[vertex.id] = weight
-                vertex.neighbors[self.id] = weight
-
-    def get_neighbors(self):
-        '''
-        获取该节点所有邻居的ID
-        '''
-        return list(self.neighbors.keys())
-
-    def __repr__(self):
-        return str(self.id) + ' connected to: ' + str([x for x in self.neighbors])
+            if vertex.name not in self._neighbors:
+                self._neighbors[vertex.name] = weight
+                vertex._neighbors[self.name] = weight
 
     def set_weight(self, vertex, weight):
         if isinstance(vertex, Vertex):
-            if vertex.id in self.neighbors:
-                self.neighbors[vertex.id] = weight
-                vertex.neighbors[self.id] = weight
+            if vertex.name in self._neighbors:
+                self._neighbors[vertex.name] = weight
+                vertex._neighbors[self.name] = weight
 
-    def get_weight(self, nbr):
-        return self.neighbors[nbr]
+    def get_weight(self, nbr_name):
+        return self._neighbors[nbr_name]
 
-    def set_properties(self, **kwargs):
-        self.properties = self.properties + kwargs
-
-    def get_properties(self):
-        return self.properties
+    def set_properties(self, properties):
+        self.properties = self.properties + properties
 
 
 # 采用邻接表存储图
 class GraphAL:
-    # 初始化图
     def __init__(self):
         self.vertices = dict()
 
     def add_vertex(self, vertex):
         if isinstance(vertex, Vertex):
-            if vertex.id not in self.vertices.keys():
-                self.vertices[vertex.id] = vertex
+            if vertex.name not in self.vertices.keys():
+                self.vertices[vertex.name] = vertex
             else:
-                print('Vertex: id=%s, already exists' % vertex.id)
-        return list(self.vertices.keys())
+                print('Vertex: name=%s, already exists' % vertex.name)
+        return list(self.vertices.keys()).sort()
 
-    def get_vertex(self, id):
-        if id in self.vertices:
-            return self.vertices[id]
+    def get_vertex(self, name):
+        if name in self.vertices:
+            return self.vertices[name]
 
     def add_edge(self, src, dst, weight=0):
         if src in self.vertices and dst in self.vertices:
             self.vertices[src].add_neighbor(self.vertices[dst], weight)
 
-    def __contains__(self, id):
-        return id in self.vertices
+    def __contains__(self, name):
+        return name in self.vertices
 
     # 迭代显示邻接表的每个顶点的邻居节点
     def __iter__(self):
         return iter(self.vertices.values())
 
     def __repr__(self):
-        for id in self.vertices:
-            print(self.vertices[id])
+        graph = ''
+        for name in self.vertices:
+            graph = graph + str(self.vertices[name]) + ' - '
+            for x in self.vertices[name].neighbors:
+                graph = graph + str(self.vertices[x]) + ', '
+            graph = graph.rstrip(', ') + '\n'
+        return graph
 
 
 if __name__ == '__main__':
-
-    # a = Vertex(1, name='A')
-    # b = Vertex(2, name='B')
-    # c = Vertex(3, name='C')
-    # d = Vertex(4, name='D')
-    # e = Vertex(5, name='E')
-
     g = GraphAL()
     for i in range(6):
         g.add_vertex(Vertex(i))
@@ -102,6 +101,8 @@ if __name__ == '__main__':
     g.add_edge(5, 4, 8)
     g.add_edge(5, 2, 1)
 
+    print(g)
+
     for v in g:
-        for w in v.get_neighbors():
-            print("(%s,%s)" % (v.id, g.get_vertex(w).id))
+        for w in v.neighbors:
+            print("(%s, %s, %s)" % (v.name, w, v.get_weight(w)))
